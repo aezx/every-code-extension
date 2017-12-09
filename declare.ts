959b628987d67595ef1ff52eecf7e5339dd10dea
Aether Lazuli <admin@athelo.net>
2017-12-09 22:10:07 +0000

    Create declare.ts

---
+++ declare.ts
@@ -0,0 +1,6 @@
+declare module arithmetics {
+    add(left: number, right: number): number;
+    subtract(left: number, right: number): number;
+    multiply(left: number, right: number): number;
+    divide(left: number, right: number): number;
+}
